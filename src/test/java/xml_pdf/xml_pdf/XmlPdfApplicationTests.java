1271c40631e1318b8011f7e39f2c8d3bc71d179a
Eliel Silva <devops@flarconsulting.com.br>
2022-06-02 15:54:17 -0300

    first commit

---
+++ src/test/java/xml_pdf/xml_pdf/XmlPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package xml_pdf.xml_pdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlPdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
